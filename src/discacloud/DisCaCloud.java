36b56dac8be904976cd0be2386fda75e29b2e809
ataka <ataka@ATAKAN-NILAY>
2016-01-24 12:20:21 +0200

    cloudsim library is added

--- src/discacloud/DisCaCloud.java
+++ src/discacloud/DisCaCloud.java
@@ -5,6 +5,7 @@
  */
 package discacloud;
 
+import org.cloudbus.cloudsim.Datacenter;
 /**
  *
  * @author ataka
@@ -17,6 +18,7 @@ public class DisCaCloud {
     public static void main(String[] args) {
         // TODO code application logic here
         // HELLO WORLD!
+        System.out.println("aaa");
     }
     
 }
