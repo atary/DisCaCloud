53f7a45acbda7805318ddaaebda3ca3a37122456
aralat@itu.edu.tr <>
2016-02-03 04:50:15 +0200

    test

--- src/discacloud/DisCaCloud.java
+++ src/discacloud/DisCaCloud.java
@@ -16,7 +16,6 @@ public class DisCaCloud {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
