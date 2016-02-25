f612e84fcaaf1d0ef2492147404b388ecf17e149
Atakan Aral <aralat@itu.edu.tr>
2016-02-25 04:10:57 +0200

    NetworkTopology.getSourceNeighbour() is implemented.

--- src/org/cloudbus/cloudsim/network/DelayMatrix_Float.java
+++ src/org/cloudbus/cloudsim/network/DelayMatrix_Float.java
@@ -5,35 +5,41 @@
  *
  * Copyright (c) 2009-2012, The University of Melbourne, Australia
  */
-
 package org.cloudbus.cloudsim.network;
 
 import java.util.Iterator;
 
 /**
- * This class represents an delay-topology storing every distance between connected nodes
- * 
+ * This class represents an delay-topology storing every distance between
+ * connected nodes
+ *
  * @author Thomas Hohnstein
  * @since CloudSim Toolkit 1.0
  */
 public class DelayMatrix_Float {
 
-	/**
-	 * matrix holding delay information between any two nodes
-	 */
-	protected float[][] mDelayMatrix = null;
+    /**
+     * matrix holding delay information between any two nodes
+     */
+    protected float[][] mDelayMatrix = null;
 
-	/**
-	 * number of nodes in the distance-aware-topology
-	 */
-	protected int mTotalNodeNum = 0;
+    /**
+     * number of nodes in the distance-aware-topology
+     */
+    protected int mTotalNodeNum = 0;
 
-	/**
-	 * private constructor to ensure that only an correct initialized delay-matrix could be created
-	 */
-	@SuppressWarnings("unused")
-	private DelayMatrix_Float() {
-	};
+    // ATAKAN: Predecessor matrix;
+    protected int[][] predecessorsMatrix = null;
+
+    /**
+     * private constructor to ensure that only an correct initialized
+     * delay-matrix could be created
+     */
+    @SuppressWarnings("unused")
+    private DelayMatrix_Float() {
+    }
+
+    ;
 
 	/**
 	 * this constructor creates an correct initialized Float-Delay-Matrix
@@ -43,102 +49,112 @@ public class DelayMatrix_Float {
 	 */
 	public DelayMatrix_Float(TopologicalGraph graph, boolean directed) {
 
-		// lets preinitialize the Delay-Matrix
-		createDelayMatrix(graph, directed);
+        // lets preinitialize the Delay-Matrix
+        createDelayMatrix(graph, directed);
 
-		// now its time to calculate all possible connection-delays
-		calculateShortestPath();
-	}
+        // now its time to calculate all possible connection-delays
+        calculateShortestPath();
+    }
 
-	/**
-	 * @param srcID the id of the source-node
-	 * @param destID the id of the destination-node
-	 * @return the delay-count between the given two nodes
-	 */
-	public float getDelay(int srcID, int destID) {
-		// check the nodeIDs against internal array-boundarys
-		if (srcID > mTotalNodeNum || destID > mTotalNodeNum) {
-			throw new ArrayIndexOutOfBoundsException("srcID or destID is higher than highest stored node-ID!");
-		}
+    /**
+     * @param srcID the id of the source-node
+     * @param destID the id of the destination-node
+     * @return the delay-count between the given two nodes
+     */
+    public float getDelay(int srcID, int destID) {
+        // check the nodeIDs against internal array-boundarys
+        if (srcID > mTotalNodeNum || destID > mTotalNodeNum) {
+            throw new ArrayIndexOutOfBoundsException("srcID or destID is higher than highest stored node-ID!");
+        }
 
-		return mDelayMatrix[srcID][destID];
-	}
+        return mDelayMatrix[srcID][destID];
+    }
 
-	/**
-	 * creates all internal necessary network-distance structures from the given graph for
-	 * similarity we assume all kommunikation-distances are symmetrical thus leads to an undirected
-	 * network
-	 * 
-	 * @param graph this graph contains all node and link information
-	 * @param directed defines to preinitialize an directed or undirected Delay-Matrix!
-	 */
-	private void createDelayMatrix(TopologicalGraph graph, boolean directed) {
+    // ATAKAN: returns the id of the neighbour of the destination over which the source message arrives
+    public int getSourceNeighbour(int sourceId, int destinationId) {
+        if (sourceId == destinationId) {
+            return sourceId;
+        }
+        return predecessorsMatrix[sourceId][destinationId];
+    }
 
-		// number of nodes inside the network
-		mTotalNodeNum = graph.getNumberOfNodes();
+    /**
+     * creates all internal necessary network-distance structures from the given
+     * graph for similarity we assume all kommunikation-distances are
+     * symmetrical thus leads to an undirected network
+     *
+     * @param graph this graph contains all node and link information
+     * @param directed defines to preinitialize an directed or undirected
+     * Delay-Matrix!
+     */
+    private void createDelayMatrix(TopologicalGraph graph, boolean directed) {
 
-		mDelayMatrix = new float[mTotalNodeNum][mTotalNodeNum];
+        // number of nodes inside the network
+        mTotalNodeNum = graph.getNumberOfNodes();
 
-		// cleanup the complete distance-matrix with "0"s
-		for (int row = 0; row < mTotalNodeNum; ++row) {
-			for (int col = 0; col < mTotalNodeNum; ++col) {
-				mDelayMatrix[row][col] = Float.MAX_VALUE;
-			}
-		}
+        mDelayMatrix = new float[mTotalNodeNum][mTotalNodeNum];
 
-		Iterator<TopologicalLink> itr = graph.getLinkIterator();
+        // cleanup the complete distance-matrix with "0"s
+        for (int row = 0; row < mTotalNodeNum; ++row) {
+            for (int col = 0; col < mTotalNodeNum; ++col) {
+                mDelayMatrix[row][col] = Float.MAX_VALUE;
+            }
+        }
 
-		TopologicalLink edge;
-		while (itr.hasNext()) {
-			edge = itr.next();
+        Iterator<TopologicalLink> itr = graph.getLinkIterator();
 
-			mDelayMatrix[edge.getSrcNodeID()][edge.getDestNodeID()] = edge.getLinkDelay();
+        TopologicalLink edge;
+        while (itr.hasNext()) {
+            edge = itr.next();
 
-			if (!directed) {
-				// according to aproximity of symmetry to all kommunication-paths
-				mDelayMatrix[edge.getDestNodeID()][edge.getSrcNodeID()] = edge.getLinkDelay();
-			}
+            mDelayMatrix[edge.getSrcNodeID()][edge.getDestNodeID()] = edge.getLinkDelay();
 
-		}
-	}
+            if (!directed) {
+                // according to aproximity of symmetry to all kommunication-paths
+                mDelayMatrix[edge.getDestNodeID()][edge.getSrcNodeID()] = edge.getLinkDelay();
+            }
 
-	/**
-	 * just calculates all pairs shortest paths
-	 */
-	private void calculateShortestPath() {
-		FloydWarshall_Float floyd = new FloydWarshall_Float();
+        }
+    }
 
-		floyd.initialize(mTotalNodeNum);
-		mDelayMatrix = floyd.allPairsShortestPaths(mDelayMatrix);
-	}
+    /**
+     * just calculates all pairs shortest paths
+     */
+    private void calculateShortestPath() {
+        FloydWarshall_Float floyd = new FloydWarshall_Float();
 
-	/**
-	 * this method just creates an string-output from the internal structures... eg. printsout the
-	 * delay-matrix...
-	 */
-	@Override
-	public String toString() {
-		StringBuffer buffer = new StringBuffer();
+        floyd.initialize(mTotalNodeNum);
+        mDelayMatrix = floyd.allPairsShortestPaths(mDelayMatrix);
+        predecessorsMatrix = floyd.getPK();
+    }
 
-		buffer.append("just a simple printout of the distance-aware-topology-class\n");
-		buffer.append("delay-matrix is:\n");
+    /**
+     * this method just creates an string-output from the internal structures...
+     * eg. printsout the delay-matrix...
+     */
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
 
-		for (int column = 0; column < mTotalNodeNum; ++column) {
-			buffer.append("\t" + column);
-		}
+        buffer.append("just a simple printout of the distance-aware-topology-class\n");
+        buffer.append("delay-matrix is:\n");
 
-		for (int row = 0; row < mTotalNodeNum; ++row) {
-			buffer.append("\n" + row);
+        for (int column = 0; column < mTotalNodeNum; ++column) {
+            buffer.append("\t" + column);
+        }
 
-			for (int col = 0; col < mTotalNodeNum; ++col) {
-				if (mDelayMatrix[row][col] == Float.MAX_VALUE) {
-					buffer.append("\t" + "-");
-				} else {
-					buffer.append("\t" + mDelayMatrix[row][col]);
-				}
-			}
-		}
+        for (int row = 0; row < mTotalNodeNum; ++row) {
+            buffer.append("\n" + row);
 
-		return buffer.toString();
-	}
+            for (int col = 0; col < mTotalNodeNum; ++col) {
+                if (mDelayMatrix[row][col] == Float.MAX_VALUE) {
+                    buffer.append("\t" + "-");
+                } else {
+                    buffer.append("\t" + mDelayMatrix[row][col]);
+                }
+            }
+        }
+
+        return buffer.toString();
+    }
 }
--- src/org/cloudbus/cloudsim/network/FloydWarshall_Float.java
+++ src/org/cloudbus/cloudsim/network/FloydWarshall_Float.java
@@ -5,12 +5,13 @@
  * Originally written by Rahul Simha
  *
  */
-
 package org.cloudbus.cloudsim.network;
+
+import org.cloudbus.cloudsim.Log;
 
 /**
  * FloydWarshall algorithm to calculate all pairs delay and predecessor matrix.
- * 
+ *
  * @author Rahul Simha
  * @author Weishuai Yang
  * @version 1.2, 6/20/2005
@@ -18,131 +19,128 @@ package org.cloudbus.cloudsim.network;
  */
 public class FloydWarshall_Float {
 
-	/**
-	 * Number of vertices (when initialized)
-	 */
-	private int numVertices;
+    /**
+     * Number of vertices (when initialized)
+     */
+    private int numVertices;
 
-	// /**
-	// * The adjacency matrix (given as input),
-	// * here I use float rather than double to save memory,
-	// * since there won't be a lot of spilting for delay,
-	// * and float is accurate enough.
-	// */
-	// private float[][] adjMatrix;
+    // /**
+    // * The adjacency matrix (given as input),
+    // * here I use float rather than double to save memory,
+    // * since there won't be a lot of spilting for delay,
+    // * and float is accurate enough.
+    // */
+    // private float[][] adjMatrix;
+    /**
+     * Matrices used in dynamic programming
+     */
+    private float[][] Dk, Dk_minus_one;
 
-	/**
-	 * Matrices used in dynamic programming
-	 */
-	private float[][] Dk, Dk_minus_one;
+    /**
+     * Matrices used in dynamic programming
+     */
+    private int[][] Pk, Pk_minus_one;
 
-	/**
-	 * Matrices used in dynamic programming
-	 */
-	private int[][] Pk, Pk_minus_one;
+    /**
+     * initialization matrix
+     *
+     * @param numVertices number of nodes
+     */
+    public void initialize(int numVertices) {
+        this.numVertices = numVertices;
 
-	/**
-	 * initialization matrix
-	 * 
-	 * @param numVertices number of nodes
-	 */
-	public void initialize(int numVertices) {
-		this.numVertices = numVertices;
+        // Initialize Dk matrices.
+        Dk = new float[numVertices][];
+        Dk_minus_one = new float[numVertices][];
+        for (int i = 0; i < numVertices; i++) {
+            Dk[i] = new float[numVertices];
+            Dk_minus_one[i] = new float[numVertices];
+        }
 
-		// Initialize Dk matrices.
-		Dk = new float[numVertices][];
-		Dk_minus_one = new float[numVertices][];
-		for (int i = 0; i < numVertices; i++) {
-			Dk[i] = new float[numVertices];
-			Dk_minus_one[i] = new float[numVertices];
-		}
+        // Initialize Pk matrices.
+        Pk = new int[numVertices][];
+        Pk_minus_one = new int[numVertices][];
+        for (int i = 0; i < numVertices; i++) {
+            Pk[i] = new int[numVertices];
+            Pk_minus_one[i] = new int[numVertices];
+        }
 
-		// Initialize Pk matrices.
-		Pk = new int[numVertices][];
-		Pk_minus_one = new int[numVertices][];
-		for (int i = 0; i < numVertices; i++) {
-			Pk[i] = new int[numVertices];
-			Pk_minus_one[i] = new int[numVertices];
-		}
+    }
 
-	}
+    /**
+     * calculates all pairs delay
+     *
+     * @param adjMatrix original delay matrix
+     * @return all pairs delay matrix
+     */
+    public float[][] allPairsShortestPaths(float[][] adjMatrix) {
+        // Dk_minus_one = weights when k = -1
+        for (int i = 0; i < numVertices; i++) {
+            for (int j = 0; j < numVertices; j++) {
+                if (adjMatrix[i][j] != 0) {
+                    Dk_minus_one[i][j] = adjMatrix[i][j];
+                    Pk_minus_one[i][j] = i;
+                } else {
+                    Dk_minus_one[i][j] = Float.MAX_VALUE;
+                    Pk_minus_one[i][j] = -1;
+                }
+                // NOTE: we have set the value to infinity and will exploit
+                // this to avoid a comparison.
+            }
+        }
 
-	/**
-	 * calculates all pairs delay
-	 * 
-	 * @param adjMatrix original delay matrix
-	 * @return all pairs delay matrix
-	 */
-	public float[][] allPairsShortestPaths(float[][] adjMatrix) {
-		// Dk_minus_one = weights when k = -1
-		for (int i = 0; i < numVertices; i++) {
-			for (int j = 0; j < numVertices; j++) {
-				if (adjMatrix[i][j] != 0) {
-					Dk_minus_one[i][j] = adjMatrix[i][j];
-					Pk_minus_one[i][j] = i;
-				} else {
-					Dk_minus_one[i][j] = Float.MAX_VALUE;
-					Pk_minus_one[i][j] = -1;
-				}
-				// NOTE: we have set the value to infinity and will exploit
-				// this to avoid a comparison.
-			}
-		}
+        // Now iterate over k.
+        for (int k = 0; k < numVertices; k++) {
 
-		// Now iterate over k.
+            // Compute Dk[i][j], for each i,j
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
+                    if (i != j) {
 
-		for (int k = 0; k < numVertices; k++) {
+                        // D_k[i][j] = min ( D_k-1[i][j], D_k-1[i][k] + D_k-1[k][j].
+                        if (Dk_minus_one[i][j] <= Dk_minus_one[i][k] + Dk_minus_one[k][j]) {
+                            Dk[i][j] = Dk_minus_one[i][j];
+                            Pk[i][j] = Pk_minus_one[i][j];
+                        } else {
+                            Dk[i][j] = Dk_minus_one[i][k] + Dk_minus_one[k][j];
+                            Pk[i][j] = Pk_minus_one[k][j];
+                        }
+                    } else {
+                        Pk[i][j] = -1;
+                    }
+                }
+            }
 
-			// Compute Dk[i][j], for each i,j
+            // Now store current Dk into D_k-1
+            for (int i = 0; i < numVertices; i++) {
+                for (int j = 0; j < numVertices; j++) {
+                    Dk_minus_one[i][j] = Dk[i][j];
+                    Pk_minus_one[i][j] = Pk[i][j];
+                }
+            }
 
-			for (int i = 0; i < numVertices; i++) {
-				for (int j = 0; j < numVertices; j++) {
-					if (i != j) {
+        } // end-outermost-for
 
-						// D_k[i][j] = min ( D_k-1[i][j], D_k-1[i][k] + D_k-1[k][j].
-						if (Dk_minus_one[i][j] <= Dk_minus_one[i][k] + Dk_minus_one[k][j]) {
-							Dk[i][j] = Dk_minus_one[i][j];
-							Pk[i][j] = Pk_minus_one[i][j];
-						} else {
-							Dk[i][j] = Dk_minus_one[i][k] + Dk_minus_one[k][j];
-							Pk[i][j] = Pk_minus_one[k][j];
-						}
-					} else {
-						Pk[i][j] = -1;
-					}
-				}
-			}
+        return Dk;
 
-			// Now store current Dk into D_k-1
-			for (int i = 0; i < numVertices; i++) {
-				for (int j = 0; j < numVertices; j++) {
-					Dk_minus_one[i][j] = Dk[i][j];
-					Pk_minus_one[i][j] = Pk[i][j];
-				}
-			}
+    }
 
-		} // end-outermost-for
+    /**
+     * gets predecessor matrix
+     *
+     * @return predecessor matrix
+     */
+    public int[][] getPK() {
+        return Pk;
+    }
 
-		return Dk;
+   
 
-	}
-
-	/**
-	 * gets predecessor matrix
-	 * 
-	 * @return predecessor matrix
-	 */
-	public int[][] getPK() {
-		return Pk;
-	}
-
-
-/*
-  public static void main (String[] argv)
+    /* public static void main (String[] argv)
   {
     // A test case.
-     *
-      double[][] adjMatrix = {
+     
+      float[][] adjMatrix = {
         {0, 1, 0, 0, 1},
         {1, 0, 1, 3, 0},
         {0, 1, 0, 2, 0},
@@ -152,7 +150,7 @@ public class FloydWarshall_Float {
 
 
       int n = adjMatrix.length;
-      FloydWarshall fwAlg = new FloydWarshall ();
+      FloydWarshall_Float fwAlg = new FloydWarshall_Float ();
       fwAlg.initialize (n);
       adjMatrix=fwAlg.allPairsShortestPaths (adjMatrix);
 
@@ -161,7 +159,7 @@ public class FloydWarshall_Float {
 	    for(int i=0;i<n;i++){
 	    	s0.append("Node "+i+" to others:");
 	    	for(int j=0;j<n;j++){
-	    			s0.append(LogFormatter.sprintf(" % 6.1f     ", adjMatrix[i][j]));
+	    			s0.append(" "+ adjMatrix[i][j]+"     ");
 
 	    	}
 	    	s0.append("\n");
@@ -177,6 +175,7 @@ public class FloydWarshall_Float {
 	    	for(int j=0;j<n;j++){
 	    		Log.print("From "+i+" to "+j+": ");
 		    	int pre=Pk[i][j];
+                        Log.print("("+fwAlg.getSourceNeighbour(i,j)+")");
 		    	while((pre!=-1)&&(pre!=i)){
 		    		Log.print(" <-  "+ pre);
 		    		pre=Pk[i][pre];
@@ -187,7 +186,5 @@ public class FloydWarshall_Float {
 		    }
 	    }
 
-  }
-
-*/
+  }*/
 }
