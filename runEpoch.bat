0d7c9cda40312cd7278c7455180a5866f0668443
Atakan Aral <aralat@itu.edu.tr>
2016-08-24 18:27:29 +0300

    removed results

--- runEpoch.bat
+++ runEpoch.bat
@@ -1,2 +1,2 @@
-FOR /L %%G IN (3100,100,4000) DO type NUL > %%G.txt
-FOR /L %%G IN (3100,100,4000) DO START java -jar dist/DisCaCloud.jar %%G 0.01 100000
+FOR /L %%G IN (4100,100,5000) DO type NUL > %%G.txt
+FOR /L %%G IN (4100,100,5000) DO START java -jar dist/DisCaCloud.jar %%G 0.01 100000
